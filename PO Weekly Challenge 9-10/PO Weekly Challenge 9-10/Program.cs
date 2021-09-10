b6286c0826ea49c6f0e927d64891347bc1aea0f6
Jeffrey Breiner <jeffreybreiner99@gmail.com>
2021-09-10 12:41:44 -0400

    Complete

---
+++ PO Weekly Challenge 9-10/PO Weekly Challenge 9-10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PO_Weekly_Challenge_9_10
+{
+    class Program
+    {
+        static string GiveMeSomething(string a)
+        {
+            return "something " + a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GiveMeSomething(Console.ReadLine()));
+        }
+    }
+}
